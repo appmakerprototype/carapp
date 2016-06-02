850dfdfccbdc1ad49a4a8cb34c86db7022a893f2
Paulo Lorenzo Basilio <paulolorenzobasilio@ITSOs-iMac-Paulo-Lorenzo-Basilio.local>
2016-06-02 16:37:40 +0800

    Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,8 +7,40 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use Validator;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+class Controller extends BaseController {
+
+    use AuthorizesRequests,
+        AuthorizesResources,
+        DispatchesJobs,
+        ValidatesRequests;
+
+    /**
+     * Echo JSON Response
+     */
+    private static function echoResponse() {
+        $json["success"] = false;
+        $json["message"] = "Required field(s) is missing or empty";
+        header('Content-type: application/json');
+        http_response_code(400);
+
+        echo json_encode($json);
+        die();
+    }
+
+    /**
+     * Custom Validator
+     * 
+     * @param Request $request - user request
+     * @param array $validate_fields - type of fields to be validate
+     */
+    public static function validator($request, Array $validate_fields) {
+        $validator = Validator::make($request->all(), $validate_fields);
+
+        if ($validator->fails()) {
+            Controller::echoResponse();
+        }
+    }
+
 }
